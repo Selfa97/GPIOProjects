deae67346389eeffa2fa21e5bc4721e12576a36b
Sam Fairs <selfa97@outlook.com>
2020-12-05 21:17:20 +1100

    Added new Admin controller that allows remote shutdown of the application. Will secure endpoint in the future... Updated Nuget packages.

---
+++ GPIOWebRunner/Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace GPIOWebRunner.Controllers
+{
+    [Route("api/admin")]
+    [ApiController]
+    public class AdminController
+    {
+        private readonly IHostApplicationLifetime _applicationLifetime;
+        private readonly ILogger<AdminController> _logger;
+
+        public AdminController(IHostApplicationLifetime applicationLifetime, ILogger<AdminController> logger)
+        {
+            _applicationLifetime = applicationLifetime;
+            _logger = logger;
+        }
+
+        [HttpGet("shutdown")]
+        public IActionResult Shutdown()
+        {
+            _logger.LogInformation("Shutting down GPIOWebRunnner");
+
+            _applicationLifetime.StopApplication();
+
+            return new OkResult();
+        }
+    }
+}
